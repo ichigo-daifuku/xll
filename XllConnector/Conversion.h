4b6bba24131255f89f00e8feb32e62c0001996ee
ichigo-daifuku <67705920+ichigo-daifuku@users.noreply.github.com>
2020-07-02 09:59:25 +0200

    Update Conversion.h
    const

--- XllConnector/Conversion.h
+++ XllConnector/Conversion.h
@@ -51,7 +51,7 @@ namespace XLL_NAMESPACE
 			"Don't know how to convert the given type to XLOPER12. "
 			"Overload CreateValue(LPXLOPER12, T) to implement it.");
 	}
-	HRESULT CreateValue(LPXLOPER12, std::vector<double>&);
+	HRESULT CreateValue(LPXLOPER12, const std::vector<double>&);
 	HRESULT CreateValue(LPXLOPER12, const XLOPER12 &);
 	HRESULT CreateValue(LPXLOPER12, double);
 	HRESULT CreateValue(LPXLOPER12, int);
